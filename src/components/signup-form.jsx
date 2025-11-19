54eefa7bbe22494a222b10472f0c4881ca852e1c
LUCAS980527 <amarbayn0527@gmail.com>
2025-11-19 14:27:04 +0800

    front1

---
+++ src/components/signup-form.jsx
@@ -0,0 +1,70 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function SignupForm({ className, ...props }) {
+  return (
+    <div
+      className={cn("flex flex-col gap-6 w-[1320px] h-[904px]", className)}
+      {...props}
+    >
+      <Card className="overflow-hidden">
+        <CardContent className="grid md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <button className="">
+
+              </button>
+              <div className="flex flex-col  gap-2 text-center">
+                <h1 className="text-2xl font-bold">Create your account</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Enter your email below to create your account
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+                <FieldDescription>
+                  We&apos;ll use this to contact you. We will not share your
+                  email with anyone else.
+                </FieldDescription>
+              </Field>
+
+              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                Or continue with
+              </FieldSeparator>
+
+              <FieldDescription className="text-center">
+                Already have an account? <a href="#">Sign in</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="./delivery.png"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  );
+}
